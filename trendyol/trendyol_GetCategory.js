94f6ed64684bfa19ad4b80f0e7c1ef37b4d8fbcc
İlayda Tiryaki <tiryaki.ilyda@gmail.com>
2022-01-23 00:01:30 +0300

    homework-2

---
+++ trendyol/trendyol_GetCategory.js
@@ -0,0 +1,8 @@
+const axios = require("axios");
+
+exports.fetchCategory = async () => {
+  const response = await axios.get("https://api.trendyol.com/sapigw/product-categories");
+  return response.data;
+};
+
+
